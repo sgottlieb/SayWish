64987953c421c8ef60c9d1812a02a4a3c1eb623e
Sara Gottlieb <gottlieb0@gmail.com>
2012-11-12 18:01:28 -0800

    collecting images and storing in static

--- saywish.py
+++ saywish.py
@@ -10,12 +10,11 @@ app.config.from_object(__name__)
 
 @app.route("/get_wishes/", methods = ["GET"])
 def get_wishes():
-	print "getting ids"
 	ids = request.args['items']
 	website = request.args['weburl']
-	print "going to scrapedoc"
-	beautiful = scrapedoc.choose_price(website, ids)
-	return render_template("get_wish.html", beautiful=beautiful)
+	beautiful = scrapedoc.first_time(website, ids)
+	images = scrapedoc.find_images(website)
+	return render_template("get_wish.html", beautiful = beautiful, images = images, website = website)
 
 
 if __name__ == "__main__":
--- scrapedoc.py
+++ scrapedoc.py
@@ -1,26 +1,45 @@
 from urllib2 import urlopen
+import urllib, cStringIO
 from bs4 import BeautifulSoup
+from PIL import Image
+from StringIO import StringIO
+import os
+
+import requests
 import unittest
 import re
 
-
-def choose_site(url, ids):
+def open_site(url):
 	webpage = urlopen(url)
 	soup = BeautifulSoup(webpage)
-	product = soup.select("body")
-	return product
+	return soup
 
-def choose_price(url, ids):
-	webpage = urlopen(url)
-	price = {}
-	soup = BeautifulSoup(webpage)
+
+def first_time(url, ids):
+	soup = open_site(url)
+	best_div = {}
 	ids = ids.split(',')
 	for specific_id in ids:
-		print specific_id
 		if specific_id:
 			product = soup.select("#%s" % (specific_id))
-			prices = re.search("\$\d+", str(product))
-			if prices:
-				price[specific_id] = prices.group()
-	return price
+			prices = re.findall("\$\d+.?\d*", str(product))
+			title_matches = re.finditer(">\s*(\w\w+\s*?\w*?)\s*?<", str(product))
+			titles = [match_obj.group(1) for match_obj in title_matches]
+			if prices or titles:
+				best_div[specific_id]=[titles, prices]
+	print best_div
+	return best_div
 
+def find_images(url):
+	images= {}
+	soup = open_site(url)
+	for image in soup.findAll("img"):
+		if image["src"].lower().startswith("http"):
+			resp = requests.get("%(src)s" % image)
+			i = Image.open(StringIO(resp.content))
+			if i.size[0] > 200 and i.size[1] > 200:
+				i.show()
+				img_id = image['id']
+				i.save('static/img/%s' % img_id, "JPEG")
+				images[image['id']]= i
+	return images
