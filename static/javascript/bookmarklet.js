d1935bf2c9a5df77d29051d34c44961f4a728c99
Sara Gottlieb <saragottlieb@Saras-MacBook-Air.local>
2012-11-30 18:28:52 -0800

    highlights divs as it captures class name

--- static/javascript/bookmarklet.js
+++ static/javascript/bookmarklet.js
@@ -1,8 +1,10 @@
 // this let's you click, mouse over and collect div ids and sends to get_wish/ on third click
-
 var jQueryLib = document.createElement("script");
 jQueryLib.src = "http://code.jquery.com/jquery-1.6.1.min.js";
 document.body.appendChild(jQueryLib);
+var jQueryUILib = document.createElement("script");
+jQueryUILib.src = "http://code.jquery.com/ui/1.9.2/jquery-ui.js";
+document.body.appendChild(jQueryUILib);
 var items = [];
 var click_listener;
 var capturing = false;
@@ -21,7 +23,9 @@ click_listener = addEventListener('click', function(){
 var mouse_listener = document.addEventListener('mouseover', function(evt) {
 	if (capturing) {
 		saywish_item = evt.target;
+		$(saywish_item).effect("highlight", {color:'#87CEFA'}, 250);
 		items.push(saywish_item.getAttribute('class'));
-		console.log('adding item')
+		console.log('adding item');
+
 	}
 });
